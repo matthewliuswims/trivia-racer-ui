f2e9b6c9c51b2efb9b61e9432b5dbc0d6825383c
Matthew <matthew@matthewliu.net>
2020-06-21 22:46:54 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from 'styled-components' // following https://www.reddit.com/r/reactjs/comments/aynfex/styled_components_share_your_folderfile_structure/
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <Header> Hi people </Header>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
@@ -18,5 +20,11 @@ const IndexPage = () => (
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
+
+// Styles
+
+const Header = styled.h1`
+  color: blue;
+`
 
 export default IndexPage
